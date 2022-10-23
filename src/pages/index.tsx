a183a11069224773c64e723d36e368bf6b7a5aa7
Noah Emmenegger <noahemmenegger.business@gmail.com>
2022-10-23 21:49:52 +0200

    Add more animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
@@ -20,11 +21,17 @@ const Home: NextPage = () => {
                         <WordsAnimation text="Turn your" replay={true} />
                         <WordsAnimation text="IT idea into" replay={true} delay={0.4} />
                         <WordsAnimation text="reality." replay={true} delay={1.4} />
-                        <Quote classname="py-10">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                            labore et dolore magna aliqua. Ut enim ad minim veniam
-                        </Quote>
-                        <Button text="learn more" />
+                        <motion.div
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            transition={{ delay: 2.4, duration: 0.5 }}
+                        >
+                            <Quote classname="py-10">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+                            </Quote>
+                            <Button text="learn more" />
+                        </motion.div>
                     </div>
                     <div className="w-2/3 h-auto relative">
                         <Image src="/images/HomeImage.svg" layout="fill" alt="Freelancer coding a website" />
