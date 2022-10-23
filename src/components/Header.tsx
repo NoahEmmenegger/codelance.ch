bc820f99c755c1a4585d28b90fe62223993c4c95
Noah Emmenegger <noahemmenegger.business@gmail.com>
2022-10-23 19:52:13 +0200

    Finish header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,14 @@ import Button from './common/Button';
 export default function Header() {
     return (
         <header>
+            <div className="absolute">
+                <svg xmlns="http://www.w3.org/2000/svg" width="383" height="125" fill="none" viewBox="0 0 383 125">
+                    <path
+                        fill="#E3E5F1"
+                        d="M-80.065 47.945c-54.766-105.71 28.167-132.167 84.5-137 90.5-14.167 260.547.595 339 64 93.249 75.363 0 128.708-67.5 142-93.251 18.363-318.25 3.863-356-69Z"
+                    />
+                </svg>
+            </div>
             <div className="flex justify-between items-center p-10">
                 <div className="w-1/3">
                     <Image src="/logo.svg" height={44} width={240} alt="Logo of Codelance GmbH" />
@@ -23,7 +31,7 @@ export default function Header() {
 
 function HeaderLink({ href, name }: { href: string; name: string }) {
     return (
-        <a href={href} className="ml-10">
+        <a href={href} className="ml-10 text-xl">
             {name}
         </a>
     );
