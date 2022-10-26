9f500d24e4b61b5b6f957ba215ff58ff53fd998d
Noah Emmenegger <noahemmenegger.business@gmail.com>
2022-10-26 23:04:24 +0200

    Add testimonials

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,8 @@ import Button from '../components/common/Button';
 import Quote from '../components/common/Quote';
 import ArrowSVG from '../components/common/svg/Arrow';
 import BlubSVG from '../components/common/svg/Blub';
+import Wave1 from '../components/common/svg/Wave1';
+import Wave2 from '../components/common/svg/Wave2';
 
 const Home: NextPage = () => {
     return (
@@ -49,8 +51,35 @@ const Home: NextPage = () => {
                         </div>{' '}
                     </div>
                 </div>
+                <div>
+                    <Wave1 />
+                    <div className="bg-secondary px-44 pb-36">
+                        <div className="flex px-28">
+                            <Testimonial />
+                            <Testimonial />
+                            <Testimonial />
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <Wave2 />
+                </div>
             </main>
         </>
+    );
+};
+
+const Testimonial = () => {
+    return (
+        <div className="w-1/3 bg-accent p-10 mx-10 rounded-2xl">
+            <b>Max Mustermann</b>
+            <p>CEO at Company</p>
+            <br />
+            <p>
+                &quot;Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                labore et dolore magna aliqua. Ut enim ad minim veniam&quot;
+            </p>
+        </div>
     );
 };
 
