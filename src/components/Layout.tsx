e7db9f99431372b3e219f85453a45a6de6fcd7f5
Noah Emmenegger <noahemmenegger.business@gmail.com>
2022-12-08 11:36:04 +0100

    Add meta tags

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,12 +7,18 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <div className={`overflow-hidden`}>
             <Head>
                 <title>Codelance GmbH</title>
+                <meta property="og:title" content="Codelance GmbH" />
                 <meta name="description" content="Codelance GmbH" />
-                <meta name="keywords" content="codelance baar, baar, zug, codelance, website, noah emmenegger" />
+                <meta property="og:description" content="Codelance GmbH" />
+                <meta name="keywords" content="codelance baar, baar, zug, codelance, website, noah Emmenegger" />
                 <link rel="icon" href="/favicon.ico" />
                 <meta name="author" content="Noah Emmenegger" />
                 <meta name="theme-color" content="#2C3359" />
-                <meta property="og:image" content="/logo.svg" />
+                <meta property="og:image" content="https://www.codelance.ch/images/HomeImage.svg" />
+                <meta property="twitter:card" content="Codelance GmbH" />
+                <meta property="twitter:title" content="Codelance GmbH" />
+                <meta property="twitter:description" content="Codelance GmbH" />
+                <meta property="twitter:image" content="https://www.codelance.ch/images/HomeImage.svg" />
             </Head>
             <Header />
             <main>{children}</main>
