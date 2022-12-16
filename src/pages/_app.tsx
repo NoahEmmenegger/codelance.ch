04484d8a21887580418f4199b172f27842c0c93f
Noah Emmenegger <noahemmenegger.business@gmail.com>
2022-12-16 21:10:41 +0100

    Add hotjar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
 import { Toaster } from 'react-hot-toast';
+import { hotjar } from 'react-hotjar';
 import nextI18NextConfig from '../../next-i18next.config';
 import Layout from '../components/Layout';
+
 import '../styles/index.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
+    useEffect(() => {
+        hotjar.initialize(3292241, 6);
+    }, []);
+
     return (
         <Layout>
             <Component {...pageProps} />
