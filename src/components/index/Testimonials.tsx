3679b3a647fcc23a00328993b816cd7041f4a08c
Noah Emmenegger <noahemmenegger.business@gmail.com>
2023-01-02 20:38:02 +0100

    Add scrolling

--- src/components/index/Testimonials.tsx
+++ src/components/index/Testimonials.tsx
@@ -1,13 +1,61 @@
 import { useTranslation } from 'next-i18next';
+import { useRef, useState } from 'react';
 
 export default function Testimonials() {
     const { t } = useTranslation('testimonials');
+    const [isDragging, setIsDragging] = useState(false);
+    const [dragStartPosition, setDragStartPosition] = useState(0);
+
+    const ref = useRef<HTMLDivElement>(null);
 
     return (
         <div className="flex flex-col items-center justify-center bg-secondary pb-10 lg:pb-20">
             <h2 className="mt-10 mb-3 text-4xl lg:text-6xl">{t('title')}</h2>
             <h3 className="lg:text-3xl">{t('subtitle')}</h3>
-            <div className="flex flex-row 2xl:mx-44">
+            <div
+                className="flex flex-row 2xl:mx-44 overflow-x-scroll w-full lg:w-4/5 snap-x scroll-smooth"
+                ref={ref}
+                onMouseDown={(e) => {
+                    e.preventDefault();
+                    setDragStartPosition(e.clientX);
+                    setIsDragging(true);
+                }}
+                onMouseUp={(e) => {
+                    e.preventDefault();
+                    setIsDragging(false);
+                }}
+                onMouseMove={(e) => {
+                    if (isDragging) {
+                        e.preventDefault();
+                        // if move left
+                        if (e.clientX < dragStartPosition) {
+                            ref.current?.scrollTo({
+                                left: ref.current.scrollLeft + 500,
+                                behavior: 'smooth',
+                            });
+                        }
+                        // if move right
+                        else {
+                            ref.current?.scrollTo({
+                                left: ref.current.scrollLeft - 500,
+                                behavior: 'smooth',
+                            });
+                        }
+                    }
+                }}
+            >
+                <Testimonial name="Unknown" position={t('1_position')}>
+                    {t('1_quote', { ns: 'testimonials' })}
+                </Testimonial>
+                <Testimonial name="Unknown" position={t('1_position')}>
+                    {t('1_quote', { ns: 'testimonials' })}
+                </Testimonial>
+                <Testimonial name="Unknown" position={t('1_position')}>
+                    {t('1_quote', { ns: 'testimonials' })}
+                </Testimonial>
+                <Testimonial name="Unknown" position={t('1_position')}>
+                    {t('1_quote', { ns: 'testimonials' })}
+                </Testimonial>
                 <Testimonial name="Unknown" position={t('1_position')}>
                     {t('1_quote', { ns: 'testimonials' })}
                 </Testimonial>
@@ -30,7 +78,7 @@ type TestimonialProps = {
 
 const Testimonial = ({ children, name, position }: TestimonialProps) => {
     return (
-        <div className="w-screen p-10 lg:w-full">
+        <div className="w-screen p-10 lg:w-1/3 flex-shrink-0 snap-start cursor-grab">
             <div className="lg:w-full h-full bg-quinary p-10 rounded-2xl">
                 <b className="text-xl">{name}</b>
                 <p className="text-xl">{position}</p>
